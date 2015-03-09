d3f32b697acd483a33f3c05fd2999c9f6cc49bba
bryanyang0528 <kenshin2004528@hotmail.com>
2015-03-09 23:32:02 +0800

    finish PSY

--- cgmh/cgmh/spiders/cgmh_psy.py
+++ cgmh/cgmh/spiders/cgmh_psy.py
@@ -2,6 +2,7 @@
 import sys
 import scrapy
 import time
+import re
 from datetime import datetime
 from scrapy.http import Request, FormRequest, TextResponse
 from scrapy.spider import Spider
@@ -50,95 +51,46 @@ class CgmhPsy(scrapy.Spider):
 					nameFull = n.xpath('.//span/text()').extract()
 					
 					if (nameFull != []):
-						item['name'] = nameFull[0]
+						name = re.sub('\d+', '', nameFull[0])
+						item['name'] = name.strip()
 						status = n.xpath('.//font/text()')[0].extract()
 						
 
 						if (status == u'(額滿)'):
 							item['full'] = '名額已滿'
+							try:
+								item['link'] = 'https://www.cgmh.org.tw/register/' + n.xpath('.//a/@href')[0].extract()
+							except Exception as e:
+								pass
+
 						elif (status == u'(停診)'):
 							continue
 
 					else:
-						item['name'] = n.xpath('.//a/text()')[0].extract()
+						name = re.sub('\d+', '', n.xpath('.//a/text()')[0].extract())
+						item['name'] = name.strip()
 						item['full'] = '可掛號' 
+						item['link'] = 'https://www.cgmh.org.tw/register/' + n.xpath('.//a/@href')[0].extract()
 					
+					if (column==0):
+						item['time'] = 'morning'
+					
+					if (column==1):
+						item['time'] = 'afternoon'
+					
+					if (column==2):
+						item['time'] = 'evening'	
 					
 					##將中文的年月日轉換成yyddmm
 					date = table[0].xpath('.//text()')[0].extract().encode('utf-8')
 					dateFormat = '%Y年%m月%d日'
 					date = time.strptime(date, dateFormat)
 					item['date'] = time.strftime("%Y%m%d", date)
+					item['crawlTime'] = unicode(datetime.now().strftime("%Y%m%d %H:%M"))
+					item['hospital'] = 'cgmh_tpe'
+					item['dept'] = 'PSY'
 
-					
 					items.append(item)
 
 
 		return items
-			
-'''
-			for n in range(len(table)-1):
-				print "n = " + str(n)
-
-				##每個row看有幾行            
-				tds = table[n+1].xpath('.//td')
-				print "tds = " + str(len(tds))
-
-				for day in range(len(tds)-2):
-					print "day = " + str(day)
-					item = NtuhItem()
-					try:
-						name = table[n+1].xpath('.//td')[day+2].xpath('.//font/text()').extract()
-						if (name == []):
-							continue
-						
-						item['name'] = name[0]
-						item['hospital'] = 'ntuh'
-						item['crawlTime'] = unicode(datetime.now().strftime("%Y%m%d %H:%M"))
-						
-						##區分成人及兒童
-						if (t < 2):
-							item['dept'] = 'PSY'
-						else:
-							item['dept'] = 'Chld_PSY'
-						
-
-						date = table[0].xpath('.//b/text()')[day+2].extract().split(" ")[0].split(".")
-						date = str(int(date[0])+1911) + date[1] + date[2]
-						item['date'] = date
-
-						##區分時段
-						if (t % 2 == 0):
-							item['time'] = 'morning'
-						else:
-							item['time'] = 'afternoon'
-
-						item['link'] = "https://reg.ntuh.gov.tw/webadministration/" + \
-							table[n+1].xpath('.//td')[day+2].xpath('.//a/@href').extract()[0]
-						
-						yield Request(item['link'], callback = self.parse_shift, meta = {'item': item})                
-						items.append(item)
-
-					except Exception as e:
-						pass
-					#print item['name']
-					
-			#return items
-
-	def parse_shift(self, response):
-
-		item = response.meta['item']
-		isFull = Selector(response).xpath('//table[@id= "DataTable"]/tr')[1].\
-			xpath('.//font/text()').extract()[0].strip()
-
-		if (isFull == u'名額已滿'):
-			item['full'] = u'名額已滿'
-		else:
-			item['full'] = u'可掛號'
-
-		return item
-	
-##output format
-
-#hospital dept date time name
-'''
